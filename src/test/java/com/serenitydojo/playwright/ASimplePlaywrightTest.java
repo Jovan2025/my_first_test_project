afc3d473c131d40cb7fd27517f0db994d0c72eb7
Jocke <aleksic.jovan.86@gmail.com>
2025-03-31 13:55:23 +0200

    added second test

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -10,6 +10,36 @@ public class ASimplePlaywrightTest {
 
     @Test
     void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+
+        browser.close();
+        playwright.close();
     }
+
+
+    @Test
+    void shouldSearchByKeyword() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+        browser.close();
+        playwright.close();
+
+    }
+
 }
