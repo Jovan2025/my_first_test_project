9b801d878983e6e7115ef47d77eee2cc1e9e1fb7
Jocke <aleksic.jovan.86@gmail.com>
2025-04-01 11:28:16 +0200

    added new anannotated test

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,35 +1,50 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+            new BrowserType.LaunchOptions()
+            .setHeadless(false)
+            .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
+        );
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void teardown() {
+        browser.close();
+        playwright.close();
+    }
 
     @Test
     void shouldShowThePageTitle() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-
         page.navigate("https://practicesoftwaretesting.com");
         String title = page.title();
 
         Assertions.assertTrue(title.contains("Practice Software Testing"));
 
-        browser.close();
-        playwright.close();
     }
 
 
     @Test
     void shouldSearchByKeyword() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-
         page.navigate("https://practicesoftwaretesting.com");
         page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
@@ -37,9 +52,7 @@ public class ASimplePlaywrightTest {
         int matchingSearchResults = page.locator(".card").count();
 
         Assertions.assertTrue(matchingSearchResults > 0);
-        browser.close();
-        playwright.close();
-
+       
     }
 
 }
